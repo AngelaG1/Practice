5202e6dd2fc989c9bae22a43431004fe80b8bbe6
AngelaG1 <agloria@ucdavis.edu>
2019-01-18 00:04:26 -0800

    Main contains the method
    Aux array B stores change according to queries and while printing A each element is the sum of itself and all values till that index from array B. This reduces dependency on the number of queries and makes it an O(1). Only O(n) is while printing.

--- biSearch/main.cpp
+++ biSearch/main.cpp
@@ -4,47 +4,59 @@
 using namespace std;
 ifstream f("data.in");
 
-int A[100],n;
+int A[100], B[100], X, n, nq, Left, Right,tempB;
 
 int biSearch(int x){
-int left = 0;
-int right = n-1;
-while(left<=right){
-    int mid = (left+right)/2;
-
-    if(A[mid]==x){
-    cout<<"Found at : ";
-    return mid;
+    int left = 0;
+    int right = n-1;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(A[mid]==x){
+            cout<<"Found at : ";
+            return mid;
+        }
+        else if(A[mid]<x){
+            left = mid+1;
+        }
+        else{
+            right = mid -1;
+        }
     }
-    else if(A[mid]<x){
-    left = mid+1;
-    }
-    else{
-    right = mid -1;
-    }
-
-}
-return -1;
-
+    return -1;
 }
 
-void addWithin(int a, int b, int X){
+void addWithin(int a, int b, int X_){
 for(int i = a; i<=b; i++){
-A[i]+=X;
-//cout<<A[i]<<" ";
+A[i]+=X_;
 }
 }
+
+
+
 int main()
 {
-    //int n;
     f>>n;
     for(int i = 0; i<n; i++)
         f>>A[i];
 
+    f>>nq;
     //cout<<biSearch(9);
-    addWithin(2,5,3);
+    //addWithin(2,5,3);
+    for(int i = 0; i<nq; i++){
+        f>>Left;
+        f>>Right;
+        f>>X;
+        B[Left]+=X;
+        B[Right+1]-=X;
 
-    for(int i = 0; i<n; i++)
-        cout<<A[i];
+    }
+
+    for(int i = 0; i<n; i++){
+        tempB+=B[i];
+        A[i]+=tempB;
+        cout<<A[i]<<" ";
+    }
+
+
     return 0;
 }
